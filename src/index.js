1e06496ca40a26f42244f88049a41358a276752a
HS-0914 <gudtn2238@gmail.com>
2025-04-13 18:32:32 +0900

    refactor(index):
    index.js initBoard 수정

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import { initUI } from "./ui.js";
 import { loadPuzzle, randomChess } from "./api.js";
 
 $(document).ready(function () {
-  initBoard("aiBoard");
-  initBoard("myBoard");
-  initBoard("puzzleBoard");
+  const boardIds = ["aiBoard", "myBoard", "puzzleBoard"];
+  for (const id of boardIds) {
+    initBoard(id);
+  }
   loadPuzzle("puzzleBoard");
   initUI();
   setTimeout(() => randomChess("aiBoard"), 3000);
