bc5082dd6cefb696ccfcc4d7e0602fab5a012b2e
손형수 <134225438+HS-0914@users.noreply.github.com>
2025-05-10 23:00:27 +0900

    Delete src/janggi.js

--- src/janggi.js
+++
@@ -1,7 +0,0 @@
-import Module from "ffish-es6";
-let ffish = null;
-
-ffish = new Module().then((loadedModule) => {
-  ffish = loadedModule;
-  console.log(`initialized ${ffish} ${loadedModule}`);
-});
